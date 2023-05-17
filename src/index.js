e82d7cd37b8ee001436af72ff6be8d61568f4569
Ankit <ankit26kushwa@gmail.comm>
2023-05-18 04:32:00 +0530

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import './style.css'
+import './style.css';
 import getscore from '../modules/getscore.js';
 import renderSubmit, { render } from '../modules/render&submit.js';
 // import setgame from '../modules/setgame.js';
-;
 
 // setgame();
 
