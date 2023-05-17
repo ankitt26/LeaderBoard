20662a0e97a2fcf995f71c1e498fb7591737c1b5
Ankit <ankit26kushwa@gmail.comm>
2023-05-18 04:27:11 +0530

    setscore, getscore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import './style.css'
 import getscore from '../modules/getscore.js';
 import renderSubmit, { render } from '../modules/render&submit.js';
 // import setgame from '../modules/setgame.js';
-import './style.css';
+;
 
 // setgame();
 
